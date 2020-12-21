df441ffcae210b5e50ef78a392f349dd956506f8
Thomas Jones <tjones29360@gmail.com>
2020-12-20 23:16:41 -0500

    created requirments.txt

--- workout.py
+++ workout.py
@@ -36,39 +36,39 @@ def questions():
 
     date = get_date()
 
-    j_jacks = input("How many Jumping Jacks did you do?:\n")
+    j_jacks = input("How many sets of Jumping Jacks did you do?:\n")
     if not j_jacks:
         j_jacks = 0
 
-    curls = input("How many Bicep Curls did you do?:\n")
+    curls = input("How many sets of Bicep Curls did you do?:\n")
     if not curls:
         curls = 0
 
-    r_curls = input("How many Reverse Curls did you do?:\n")
+    r_curls = input("How many sets of Reverse Curls did you do?:\n")
     if not r_curls:
         r_curls = 0
 
-    h_curls = input("How many Hammer Curls did you do?:\n")
+    h_curls = input("How many sets of Hammer Curls did you do?:\n")
     if not h_curls:
         h_curls = 0
 
-    pushups = input("How many Pushups did you do?:\n")
+    pushups = input("How many sets of Pushups did you do?:\n")
     if not pushups:
         pushups = 0
 
-    ovr_curls = input("How many Overhead Curls did you do?:\n")
+    ovr_curls = input("How many sets of Overhead Curls did you do?:\n")
     if not ovr_curls:
         ovr_curls = 0
 
-    windmills = input("How many Windmills did you do?:\n")
+    windmills = input("How many sets of Windmills did you do?:\n")
     if not windmills:
         windmills = 0
 
-    w_pushups = input("How many Wall Pushups did you do?:\n")
+    w_pushups = input("How many sets of Wall Pushups did you do?:\n")
     if not w_pushups:
         w_pushups = 0
 
-    squats = input("How many Squats did you do:?\n")
+    squats = input("How many sets of Squats did you do:?\n")
     if not squats:
         squats = 0
 
@@ -104,4 +104,5 @@ def questions():
         )
 
 
-questions()
+if __name__ == "__main__":
+    questions()
